954c1df6a3ba3c8d2daa87d796210c0e2271ea15
emmanuel <sunartio.emmanuel@gmail.com>
2022-12-04 17:59:01 +1030

    prevent DateViewHolders from getting swiped

--- app/src/main/java/com/example/sectionedrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/sectionedrecyclerview/MainActivity.kt
@@ -2,20 +2,23 @@ package com.example.sectionedrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var list: RecyclerView
+    lateinit var rv: RecyclerView
+    lateinit var list: ArrayList<PojoOfJsonArray>
+    lateinit var consolidatedList: ArrayList<ListItem>
+    lateinit var adapter: Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        list = findViewById(R.id.list)
+        rv = findViewById(R.id.rv)
 
         val c1 = Calendar.getInstance()
         c1.set(2017, 3, 23)
@@ -24,23 +27,24 @@ class MainActivity : AppCompatActivity() {
         val c3 = Calendar.getInstance()
         c3.set(2018, 4, 2)
 
-        val myOptions = listOf(
-            PojoOfJsonArray("name 1", c1, "20170323"),
-            PojoOfJsonArray("name 2", c1, "20170323"),
-            PojoOfJsonArray("name 3", c1, "20170323"),
-            PojoOfJsonArray("name 3", c2, "20170327"),
-            PojoOfJsonArray("name 3", c2, "20170327"),
-            PojoOfJsonArray("name 3", c2, "20170327"),
-            PojoOfJsonArray("name 3", c1, "20170323"),
-            PojoOfJsonArray("name 2", c3, "20180402"),
-            PojoOfJsonArray("name 3", c2, "20170327")
-        ).sortedBy { it.date } // sort by date (ascending)
+        list = arrayListOf()
+        list.apply {
+            add(PojoOfJsonArray("name 1", c1, "20170323"))
+            add(PojoOfJsonArray("name 3", c1, "20170323"))
+            add(PojoOfJsonArray("name 3", c2, "20170327"))
+            add(PojoOfJsonArray("name 3", c2, "20170327"))
+            add(PojoOfJsonArray("name 3", c2, "20170327"))
+            add(PojoOfJsonArray("name 3", c1, "20170323"))
+            add(PojoOfJsonArray("name 2", c3, "20180402"))
+            add(PojoOfJsonArray("name 3", c2, "20170327"))
+        }
+        list.sortBy { it.date }
 
-        val groupedMap: Map<String, List<PojoOfJsonArray>> = myOptions.groupBy {
+        val groupedMap: Map<String, List<PojoOfJsonArray>> = list.groupBy {
             it.dateString
         } // creates a map of 'date' to a 'list of PojoOfJsonArray' - eg: key '20160605', value is a list containing 'name 2', 'name 3'
 
-        val consolidatedList = mutableListOf<ListItem>()
+        consolidatedList = arrayListOf()
         for (date:String in groupedMap.keys) {
             consolidatedList.add(DateItem(date)) // creates a DateItem class for each 'date' in groupedMap
             val groupItems: List<PojoOfJsonArray>? = groupedMap[date] // groupItems is a list of PojoOfJsonArray which corresponds to the above 'date'
@@ -49,7 +53,32 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val adapter = Adapter(consolidatedList)
-        list.adapter = adapter
+        adapter = Adapter(consolidatedList)
+        rv.adapter = adapter
+        swipeFunctions()
     }
+
+    private fun swipeFunctions() {
+        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun getSwipeDirs (recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
+                if (viewHolder is Adapter.DateViewHolder) return 0 // prevents DateViewHolders from getting swiped
+                return super.getSwipeDirs(recyclerView, viewHolder)
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                consolidatedList.removeAt(viewHolder.adapterPosition)
+                adapter.notifyItemRemoved(viewHolder.adapterPosition)
+            }
+
+        }).attachToRecyclerView(rv)
+    }
+
 }
